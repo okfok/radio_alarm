fe46ce533181fac207f61f86ebbc650cc40eb1ac
okf0k <lubomvis@gmail.com>
2024-04-25 12:24:44 +0300

    fix: shell popups in powershell action

--- models.py
+++ models.py
@@ -184,7 +184,8 @@ class WinAppPSShortcutAction(AlertAction):
                         [Microsoft.VisualBasic.Interaction]::AppActivate("{self.window_name}");
                         """,
                     stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE
+                    stderr=asyncio.subprocess.PIPE,
+                    shell=False
                 )
             ).communicate()
 
@@ -199,7 +200,8 @@ class WinAppPSShortcutAction(AlertAction):
                         [System.Windows.Forms.SendKeys]::SendWait('{self.shortcut[event.alert.type][event.alert_type]}')
                         """,
                     stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE
+                    stderr=asyncio.subprocess.PIPE,
+                    shell=False
                 )
             ).communicate()
 
